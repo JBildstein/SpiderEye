eae0d9f538773e3b078cefabc21e743e2e3d4241
Johannes Bildstein <johannes@bildstein.at>
2019-01-08 22:48:56 +0100

    add missing xml documentation

--- Source/SpiderEye/Windows/IWpfWebview.cs
+++ Source/SpiderEye/Windows/IWpfWebview.cs
@@ -2,9 +2,19 @@
 
 namespace SpiderEye.Windows
 {
+    /// <summary>
+    /// Represents a WPF webview.
+    /// </summary>
     internal interface IWpfWebview : IWebview
     {
+        /// <summary>
+        /// Fires when the title within the webview was changed.
+        /// </summary>
         event EventHandler<string> TitleChanged;
+
+        /// <summary>
+        /// Gets the webview control.
+        /// </summary>
         object Control { get; }
     }
 }
--- Source/SpiderEye/Windows/ScriptInterface.cs
+++ Source/SpiderEye/Windows/ScriptInterface.cs
@@ -3,6 +3,10 @@ using System.Runtime.InteropServices;
 
 namespace SpiderEye.Windows.Internal
 {
+    /// <summary>
+    /// Script interface for the WPF WebBrowser control.
+    /// Not meant for public consumption.
+    /// </summary>
     [ComVisible(true)]
     public class ScriptInterface
     {
@@ -12,6 +16,10 @@ namespace SpiderEye.Windows.Internal
         {
         }
 
+        /// <summary>
+        /// Title change hook.
+        /// </summary>
+        /// <param name="title">The changed title.</param>
         public void ChangeTitle(string title)
         {
             TitleChanged?.Invoke(this, title);
