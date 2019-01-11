816315cfc9f830f0c280193bbd5faaf3cb20d258
Johannes Bildstein <johannes@bildstein.at>
2019-01-11 23:46:38 +0100

    fix title script invokes

--- Source/SpiderEye/Tools/Scripting/ScriptHandler.cs
+++ Source/SpiderEye/Tools/Scripting/ScriptHandler.cs
@@ -18,21 +18,29 @@ namespace SpiderEye.Tools.Scripting
 
         public void HandleScriptCall(string data)
         {
-            var info = JsonConverter.Deserialize<InvokeInfoModel>(data);
-            if (info != null)
+            try
             {
-                if (info.Type == "title") { TitleChanged?.Invoke(this, info.Parameters); }
-                else if (info.Type == "api")
+                var info = JsonConverter.Deserialize<InvokeInfoModel>(data);
+                if (info != null)
                 {
-                    var result = ApiResolver.ResolveCall(info.Id, info.Parameters);
-                    EndApiCall(info, result);
-                }
-                else if (info.CallbackId != null)
-                {
-                    string message = $"Invalid invoke type \"{info.Type ?? "<null>"}\"";
-                    EndApiCall(info, ApiResultModel.FromError(message));
+                    if (info.Type == "title")
+                    {
+                        string title = JsonConverter.Deserialize<string>(info.Parameters);
+                        TitleChanged?.Invoke(this, title);
+                    }
+                    else if (info.Type == "api")
+                    {
+                        var result = ApiResolver.ResolveCall(info.Id, info.Parameters);
+                        EndApiCall(info, result);
+                    }
+                    else if (info.CallbackId != null)
+                    {
+                        string message = $"Invalid invoke type \"{info.Type ?? "<null>"}\"";
+                        EndApiCall(info, ApiResultModel.FromError(message));
+                    }
                 }
             }
+            catch { /* TODO: do something with this */ }
         }
 
         private void EndApiCall(InvokeInfoModel info, ApiResultModel result)
