cfef0f24bdf11dee3b2bf0ac0e7a7a2827265a5c
Johannes Bildstein <johannes@bildstein.at>
2019-01-28 01:31:13 +0100

    use bridge in SPA example

--- Examples/SpiderEye.Example.Spa/Angular/app/app.component.ts
+++ Examples/SpiderEye.Example.Spa/Angular/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {
+    MessageBox,
+    MessageBoxButtons,
+    SaveFileDialog,
+    OpenFileDialog,
+    BrowserWindow,
+    DialogResult,
+    FileResult,
+} from 'spidereye';
 
 @Component({
     selector: 'app-root',
@@ -7,11 +16,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
+    messageBoxResult: DialogResult;
+    saveFileResult: FileResult;
+    openFileResult: FileResult;
+
     ngOnInit() {
         let count = 0;
         setInterval(function () {
             count++;
             document.title = 'Title Changes: ' + count;
         }, 1000);
+    }
+
+    openMessageBox() {
+        this.messageBoxResult = null;
+
+        var box = new MessageBox();
+        box.title = "Hello World";
+        box.message = "Hello World from the Webview";
+        box.buttons = MessageBoxButtons.OkCancel;
+
+        box.show(result => this.messageBoxResult = result);
+    }
+
+    openSaveFileDialog() {
+        this.saveFileResult = null;
+
+        var dialog = new SaveFileDialog();
+        dialog.title = "Hello World";
+
+        dialog.show(result => this.saveFileResult = result);
+    }
+
+    openOpenFileDialog() {
+        this.openFileResult = null;
+
+        var dialog = new OpenFileDialog();
+        dialog.title = "Hello World";
+
+        dialog.show(result => this.openFileResult = result);
+    }
+
+    openWindow() {
+        var window = new BrowserWindow({
+            title: "Hello World",
+            backgroundColor: "#FFFFFF",
+            canResize: true,
+            width: 900,
+            height: 600,
+            useBrowserTitle: true,
+            url: "/index.html"
+        });
+
+        window.show();
     }
 }
