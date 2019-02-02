555d87b0e21c265522097837a7f260bdbcfaa224
Johannes Bildstein <info@fotostein.at>
2019-02-02 21:11:27 +0100

    add struct layout to GSList

--- Source/SpiderEye/UI/Linux/Interop/GSList.cs
+++ Source/SpiderEye/UI/Linux/Interop/GSList.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Runtime.InteropServices;
 
 namespace SpiderEye.UI.Linux.Interop
 {
+    [StructLayout(LayoutKind.Sequential)]
     internal unsafe struct GSList
     {
         public readonly IntPtr Data;
