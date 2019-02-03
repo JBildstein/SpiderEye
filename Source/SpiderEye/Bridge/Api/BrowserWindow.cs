3b4f9587a25c7919a1bc71ec318a5286b8232e81
Johannes Bildstein <johannes@bildstein.at>
2019-02-03 21:05:25 +0100

    don't close child windows on parent close
    it's difficult to handle correctly and doesn't always make sense

--- Source/SpiderEye/Bridge/Api/BrowserWindow.cs
+++ Source/SpiderEye/Bridge/Api/BrowserWindow.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using SpiderEye.Bridge.Models;
 using SpiderEye.UI;
 
@@ -10,14 +9,10 @@ namespace SpiderEye.Bridge.Api
         private readonly IWindow parent;
         private readonly IWindowFactory windowFactory;
 
-        private readonly List<IWindow> windows = new List<IWindow>();
-
         public BrowserWindow(IWindow parent, IWindowFactory windowFactory)
         {
             this.parent = parent ?? throw new ArgumentNullException(nameof(parent));
             this.windowFactory = windowFactory ?? throw new ArgumentNullException(nameof(windowFactory));
-
-            parent.Closing += Parent_Closing;
         }
 
         public void Show(BrowserWindowConfigModel config)
@@ -25,17 +20,8 @@ namespace SpiderEye.Bridge.Api
             var window = windowFactory.CreateWindow(config);
             window.LoadUrl(config.Url);
             window.Show();
-            windows.Add(window);
 
             // TODO: somehow send events from this window back to webview
-        }
-
-        private void Parent_Closing(object sender, EventArgs e)
-        {
-            foreach (var window in windows)
-            {
-                window.Close();
-            }
         }
     }
 }
