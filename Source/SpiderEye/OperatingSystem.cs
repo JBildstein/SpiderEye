bb33152b035c5ecec93c407d8e7a3ba7f72f316d
Johannes Bildstein <johannes@bildstein.at>
2019-05-02 18:51:30 +0200

    change OperatingSystem enum values to be used as flags

--- Source/SpiderEye/OperatingSystem.cs
+++ Source/SpiderEye/OperatingSystem.cs
@@ -1,14 +1,17 @@
 #pragma warning disable SA1602, CS1591 // Enumeration items should be documented
 
+using System;
+
 namespace SpiderEye
 {
     /// <summary>
     /// Operating system.
     /// </summary>
+    [Flags]
     public enum OperatingSystem
     {
-        Windows,
-        MacOSX,
-        Unix,
+        Windows = 1 << 0,
+        MacOSX = 1 << 1,
+        Unix = 1 << 2,
     }
 }
