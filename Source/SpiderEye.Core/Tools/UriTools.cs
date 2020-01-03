6f9df10b4ec2c16aab821cb7e516dfc446c71622
Johannes Bildstein <johannes@bildstein.at>
2020-01-03 19:43:01 +0100

    use crypto RNG for random resource URL generation

--- Source/SpiderEye.Core/Tools/UriTools.cs
+++ Source/SpiderEye.Core/Tools/UriTools.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using System.Security.Cryptography;
 
 namespace SpiderEye.Tools
 {
     internal static class UriTools
     {
-        private static readonly Random Rng = new Random(); // TODO: use crypto RNG
+        private static readonly RandomNumberGenerator Rng = RandomNumberGenerator.Create();
 
         public static string GetRandomResourceUrl(string scheme)
         {
@@ -14,11 +14,23 @@ namespace SpiderEye.Tools
         private static string CreateRandomString(int length)
         {
             const string possible = "0123456789abcdefghijklmnopqrstuvwxyz";
+            const int range = 35; // == possible.Length - 1
+            const byte mask = 0b111111; // bits needed for range
+
             char[] result = new char[length];
+            byte[] buffer = new byte[1];
 
             for (int i = 0; i < result.Length; i++)
             {
-                result[i] = possible[Rng.Next(0, possible.Length)];
+                int charPos;
+                do
+                {
+                    Rng.GetBytes(buffer);
+                    charPos = mask & buffer[0];
+                }
+                while (charPos > range);
+
+                result[i] = possible[charPos];
             }
 
             return new string(result);
